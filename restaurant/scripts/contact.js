43b1754eedc7e707513b640ccc3384296a3d2254
Shelly Isaacs <shellyanneisaacs@gmail.com>
2019-01-16 15:32:01 -0500

    restaurant changes

--- restaurant/scripts/contact.js
+++ restaurant/scripts/contact.js
@@ -1,7 +1,8 @@
-function validateItems() {
-  if (document.input == "")
-    {return false;}
-  else if (document.textarea == "")
-    {return false;}
-  else {return true;}
-}
+// I am still unclear about how to do this validation.
+// Currently, I think it is only validating the name field.
+function validateForm() {
+    var inpObj = document.getElementById("name");
+    if (!inpObj.checkValidity()) {
+      document.getElementById("nameLabel").innerHTML = inpObj.validationMessage;
+    }
+  }
